1f42b5a1bed063e321b871e8ce6f453afff386d3
luoxin <luoxin527@outlook.com>
2023-01-04 22:53:55 +0800

    add restart container

--- server/handles/v2ray.go
+++ server/handles/v2ray.go
@@ -3,6 +3,7 @@ package handles
 import (
 	"encoding/base64"
 	"encoding/json"
+	"errors"
 	"io/fs"
 	"io/ioutil"
 	"log"
@@ -14,6 +15,7 @@ import (
 	"time"
 
 	"github.com/alist-org/alist/v3/internal/conf"
+	"github.com/alist-org/alist/v3/pkg/utils"
 	"github.com/alist-org/alist/v3/server/common"
 	"github.com/gin-gonic/gin"
 )
@@ -31,24 +33,203 @@ type V2rayConfig struct {
 	LastUpdateTime uint64   `json:"last_update_time"`
 }
 
-func ListV2rayServers(c *gin.Context) {
+type V2rayLog struct {
+	Access   string `json:"access"`
+	Error    string `json:"error"`
+	LogLevel string `json:"loglevel"`
+}
 
+type V2rayInbound struct {
+	Port     int         `json:"port"`
+	Listen   string      `json:"listen"`
+	Protocol string      `json:"protocol"`
+	Settings interface{} `json:"settings"`
+	Tag      string      `json:"tag"`
+}
+
+type V2rayOutbound struct {
+	Protocol    string      `json:"protocol"`
+	SendThrough string      `json:"sendThrough"`
+	Settings    interface{} `json:"settings"`
+	Tag         string      `json:"tag"`
+}
+
+type V2rayDns struct {
+	Servers []string `json:"servers"`
+}
+
+type V2rayApi struct {
+	Services []string `json:"services"`
+	Tag      string   `json:"tag"`
+}
+
+type V2rayPolicy struct {
+	System map[string]bool `json:"systems"`
+}
+
+type V2rayRouting struct {
+	DomainStrategy string `json:"domainStrategy"`
+}
+
+type V2rayRoutingRule struct {
+	InboundTag  []string `json:"inboundTag"`
+	OutboundTag string   `json:"outboundTag"`
+	Type        string   `json:"type"`
+	Domain      []string `json:"domain"`
+	Ip          []string `json:"ip"`
+	Port        string   `json:"port"`
+	Network     string   `json:"network"`
+	Source      []string `json:"source"`
+	User        []string `json:"user"`
+	Protocol    []string `json:"protocol"`
+	Attrs       string   `json:"attrs"`
+	BalancerTag string   `json:"balancerTag"`
+}
+
+type V2rayRoutingBalancer struct {
+	Tag      string   `json:"tag"`
+	Selector []string `json:"selector"`
+}
+
+type V2rayServerConfig struct {
+	Log       V2rayLog        `json:"log"`
+	Api       V2rayApi        `json:"api"`
+	Dns       V2rayDns        `json:"dns"`
+	Routing   V2rayRouting    `json:"routing"`
+	Policy    V2rayPolicy     `json:"policy"`
+	Inbounds  []V2rayInbound  `json:"inbounds"`
+	Outbounds []V2rayOutbound `json:"outbounds"`
+}
+
+func ListV2rayServers(c *gin.Context) {
+	conf_dir := conf.Conf.V2rayConfigDir
+	v2ray_servers_file := filepath.Join(conf_dir, "servers.json")
+	_, err := os.Stat(v2ray_servers_file)
+	if os.IsNotExist(err) {
+		common.ErrorResp(c, err, 500)
+		return
+	}
+
+	servers_bytes, err := ioutil.ReadFile(v2ray_servers_file)
+	if err != nil {
+		common.ErrorResp(c, err, 500)
+		return
+	}
+	servers := []V2RayServer{}
+	err = json.Unmarshal(servers_bytes, &servers)
+	if err != nil {
+		common.ErrorResp(c, err, 500)
+		return
+	}
+
+	common.SuccessResp(c, servers)
 }
 
 func ApplyV2RayServer(c *gin.Context) {
+	var req V2RayServer
+	if err := c.ShouldBind(&req); err != nil {
+		common.ErrorResp(c, err, 400)
+		return
+	}
 
+	config, err := loadV2rayServerConfigFromFile()
+	if err != nil {
+		common.ErrorResp(c, err, 500)
+		return
+	}
+
+	settings := []map[string]interface{}{
+		{
+			"address":  req.Address,
+			"method":   req.Method,
+			"ota":      false,
+			"password": req.Password,
+			"port":     req.Port,
+		},
+	}
+
+	for index := range config.Outbounds {
+		if config.Outbounds[index].Protocol == "shadowsocks" {
+			config.Outbounds[index].Settings = settings
+		}
+	}
+
+	saveV2rayServerConfigTo()
+	err = restartV2rayServer()
+	if err != nil {
+		common.ErrorResp(c, err, 500)
+		return
+	}
+	common.SuccessResp(c, "ok")
 }
 
 func RestartV2RayServer(c *gin.Context) {
+	err := restartV2rayServer()
+	if err != nil {
+		common.ErrorResp(c, err, 500)
+		return
+	}
 
+	common.SuccessResp(c, "ok")
+}
+
+func restartV2rayServer() error {
+	docker, err := utils.GetDefaultDocker()
+	if err != nil {
+		return err
+	}
+
+	containers, err := docker.ListContainers()
+	if err != nil {
+		return err
+	}
+	for _, container := range containers {
+		if container.Image == "" {
+			err := docker.RestartContainer(container.ID)
+			if err != nil {
+				return err
+			}
+			return nil
+		}
+	}
+
+	return errors.New("v2ray container not found")
 }
 
 func StopV2rayServer(c *gin.Context) {
 
 }
 
-func SubscribeToUrl(c *gin.Context) {
+func loadV2rayServerConfigFromFile(path string) (*V2rayServerConfig, error) {
+	configBytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	config := new(V2rayServerConfig)
+	err = json.Unmarshal(configBytes, config)
+	if err != nil {
+		return nil, err
+	}
+	return config, nil
+}
 
+func saveV2rayServerConfigTo(path string, config V2rayServerConfig) error {
+	bytes, err := json.Marshal(config)
+	if err != nil {
+		return err
+	}
+
+	err = os.Truncate(path, 0)
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(path, bytes, fs.ModeAppend)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func GetV2RayServerNetworkFlow(c *gin.Context) {
@@ -131,6 +312,7 @@ func getServersFromUrl(urlString string) []V2RayServer {
 		methodAndPassword := strings.Split(string(methodAndPasswordBytes), ":")
 
 		server := V2RayServer{
+			Id:       parsedUrl.Host,
 			Address:  hostAndPort[0],
 			Port:     hostAndPort[1],
 			Method:   methodAndPassword[0],
@@ -138,10 +320,9 @@ func getServersFromUrl(urlString string) []V2RayServer {
 		}
 		servers = append(servers, server)
 	}
-
 	return servers
 }
 
 func UpdateSubscribeUrls(c *gin.Context) {
-
+	
 }
